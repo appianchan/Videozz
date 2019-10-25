618a72aad74d51a63439bebc831d2bb68117fd72
Anthony Chan <appianchan@gmail.com>
2019-10-25 03:42:16 -0700

    created seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Video.destroy_all
+
+User.create!(username: "hunter12", password: "abcdefg")
+Video.create!(title: "First Video", description: "let's show this video", user_id: 1, view_count: 3)
+
+v = Video.first
+v.video_attatchment.attach(io: "/User/anthonychan/Desktop/test_video.mov", filename: "test_video.mov")
