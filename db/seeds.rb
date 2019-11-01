6bf10bc659da8477303e82886cc91ad4808d2375
Anthony Chan <appianchan@gmail.com>
2019-11-01 11:36:21 -0700

    changed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,18 +12,18 @@ Video.destroy_all
 
 User.create!(username: "hunter12", password: "abcdefg")
 u = User.first
-Video.create!(title: "First Video", description: "let's show this video", user_id: u.id, view_count: 3)
+Video.create!(title: "First Video", description: "let's show this video", user_id: 1, view_count: 3)
 
 v = Video.first
 file = open('https://videozz-test-bucket.s3-us-west-1.amazonaws.com/test_video.mov')
 v.video_attatchment.attach(io: file, filename: "test_video.mov")
 
-Video.create!(title: "Second Video", description: "This is the awesomest video ever", user_id: u.id, view_count: 5)
+Video.create!(title: "Second Video", description: "This is the awesomest video ever", user_id: 2, view_count: 5)
 c = Video.last
 file = open('https://videozz-test-bucket.s3-us-west-1.amazonaws.com/vid_2.mov')
 c.video_attatchment.attach(io: file, filename: "test_video_2.mov")
 
-Video.create!(title: "Third Video", description: "This video is better than the last", user_id: u.id, view_count: 10)
+Video.create!(title: "Third Video", description: "This video is better than the last", user_id: 3, view_count: 10)
 d = Video.last
 file = open('https://videozz-test-bucket.s3-us-west-1.amazonaws.com/vid_3.mov')
 d.video_attatchment.attach(io: file, filename: "test_video_3.mov")
