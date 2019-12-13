8b47f2f12fee7d1ac2ef3b5d894679d011b555fd
Anthony Chan <appianchan@gmail.com>
2019-12-13 13:34:00 -0800

    reviews

--- frontend/components/videos/reviews.js
+++ frontend/components/videos/reviews.js
@@ -11,47 +11,30 @@ export default class Reviews extends React.Component {
             video: this.props.video,
             edit: false
         };
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
     // componentDidMount() {
 
     //     this.props.update(this.state.video);
 
     // }
-    handleInput(type) {
-        return e => {
-            this.setState({ [type]: e.target.value });
-        };
-    }
-
 
     handleSubmit(e) {
+        e.preventDefault();
         
     }
-    addReview(){
-
-    }
+    
 
     render() {
-        // if (this.state.edit === false){
-            
-        // } else {
-        //     this.setState(state => ({
-        //         edit: true
-        //     }));
-        //     return (
-        //         <div>
-        //             <div className="show-likes" onClick={this.handleClick}>Likes: {this.state.likes.length}</div>
-        //         </div>
-        //     )
-        // }
+       
         return(
             <form onSubmit={this.handleSubmit}>
                 <input type="review"
                     // value="Write a review here"
-                    onChange={this.handleInput('review')}
+                    // onChange={this.handleInput('review')}
                     className="review"
                 />
-                <input className="review-submit" type="submit" value="submit" />
+                <input className="review-submit" type="submit" value="comment" />
            </form>
 
         );
