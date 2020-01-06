36186a69ffd58566a31489514cc0989078501ba4
Anthony Chan <appianchan@gmail.com>
2020-01-05 17:32:50 -0800

    reset css in html and body

--- frontend/components/videos/video_index_navbar.js
+++ frontend/components/videos/video_index_navbar.js
@@ -17,10 +17,26 @@ export default class VideoIndexNavbar extends React.Component {
     render(){
         return(
             <div className="index-navbar">
-                <i class="fas fa-home"></i>
-                <i class="fas fa-fire"></i>
-                <i class="fab fa-youtube-square">Subscriptions</i>
-                <i class="fas fa-folder"></i>
+                <div className="home">
+                    <i class="fas fa-home"></i>
+                    <div className="home-icon-text">Home</div>
+                </div>
+                <div className="trending">
+                    <i class="fas fa-fire"></i>
+                    <div className="trending-icon-text">Trending</div>
+                </div>
+                <div className="subscribe">
+                    <i class="fab fa-youtube-square"></i>
+                    <div className="subscribe-icon-text"> Subscriptions</div>
+                </div>
+                <div className="Library">
+                    <i class="fas fa-folder"></i>
+                    <div className="library-icon-text">Library</div>
+                </div>
+                
+                
+                
+                
             </div>
         )
     }
