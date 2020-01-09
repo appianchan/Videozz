acff4c44276186e010764cbf64640f16ee88ea2a
Anthony Chan <appianchan@gmail.com>
2020-01-08 16:19:44 -0800

    redecorated left-navbar in index

--- frontend/components/personal/personal.js
+++ frontend/components/personal/personal.js
@@ -10,9 +10,7 @@ class Personal extends React.Component {
     render() {
         return(
             <div>
-                <a href="https://www.linkedin.com/in/anthony-chan-02381a81/"><i class="fab fa-linkedin"></i></a>
-                <a href="https://angel.co/anthony-chan-26"><i class="fab fa-angellist"></i></a>
-                <a href="https://github.com/appianchan"><i class="fab fa-github"></i></a>
+              
             </div>
         
         )
