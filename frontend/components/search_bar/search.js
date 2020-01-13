e61563a13ab9743eeb5ae03b411b94e318dea1c6
Anthony Chan <appianchan@gmail.com>
2020-01-13 02:01:56 -0800

    fixing object error

--- frontend/components/search_bar/search.js
+++ frontend/components/search_bar/search.js
@@ -6,9 +6,10 @@ import { Link, Route } from 'react-router-dom';
 export default class Search extends React.Component {
     constructor(props){
         super(props);
-        debugger;
+        // debugger;
         this.state = {
             search: this.props.location.state.search || "",
+            search_result: []
         }
     }
 
@@ -17,11 +18,26 @@ export default class Search extends React.Component {
     }
    
     render() {
-
-
-        const videos = this.props.videos.map(video =>
-
-            // console.log(video)
+        if (this.props.videos[0] === undefined) {
+            return null;
+        }
+        // debugger;
+        
+        // debugger;
+        this.props.videos.forEach(video => {
+            if (video.title.length > this.state.search.length){
+                for(let i = 0; i <= (video.title.length - this.state.search.length); i++){
+                    const x = video.title.substring(i, i + this.state.search.length);
+                    if(x === this.state.search){
+                        this.state.search_result.push(video);
+                    }
+                }
+            }
+            
+            
+        })
+        
+        this.state.search_result.map(video =>
             <li className="video-index-element-container" key={video.id}>
                 <Link
                     className="thumbnail"
@@ -50,13 +66,48 @@ export default class Search extends React.Component {
 
                 </Link>
             </li>
+        )
+        
+
+        // const videos = this.props.videos.map(video =>
+
+        //     // console.log(video)
+        //     <li className="video-index-element-container" key={video.id}>
+        //         <Link
+        //             className="thumbnail"
+        //             to={`/videos/${video.id}`}
+        //             key={video.id}
+        //         >
+        //             <video className="thumbnail" src={`${video.videoUrl}`}></video>
+        //         </Link>
+
+
+        //         <Link
+        //             className="video-link"
+        //             to={`/videos/${video.id}`}
+        //             key={video.id}
+        //         >
+        //             <i class="fas fa-user-circle"></i>
+        //             <div className="index-video-info">
+        //                 <div className="video-index-title">{video.title} </div>
+        //                 <div className="video-index-creator">{video.creator}</div>
+        //                 <div className="video-index-bottom-element">
+        //                     <div className="video-index-viewcount">{video.view_count} views ·</div>
+        //                     <div className="video-index-datecreated">{video.date_created}</div>
+        //                 </div>
+
+        //             </div>
+
+        //         </Link>
+        //     </li>
 
 
 
-        );
-        if (this.props.videos[0] === undefined) {
-            return null;
-        }
+        // );
+        // if (this.props.videos[0] === undefined) {
+        //     return null;
+        // }
+        debugger;
         return (
             <div className="index-page">
 
@@ -67,7 +118,7 @@ export default class Search extends React.Component {
                     {/* <img src={`${this.props.videos[0].videoUrl}`} /> */}
                     <ul className="video-index-list">
                         {/* <img className="thumbnail" src="https://cdn.britannica.com/45/5645-050-B9EC0205/head-treasure-flower-disk-flowers-inflorescence-ray.jpg" /> */}
-                        {videos}
+                        {this.state.search_result}
                     </ul>
 
                 </section>
