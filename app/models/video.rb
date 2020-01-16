2765e53cc1630a88e61c1620f06bd406bebc85b2
Anthony Chan <appianchan@gmail.com>
2020-01-16 08:22:11 -0800

    created basis for uploading a video

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,8 +1,14 @@
 class Video < ApplicationRecord 
     validates :description, :title, presence: true
     # validates :title, uniqueness: true, presence: true
+    validate :ensure_video
 
     belongs_to :user
     has_one_attached :video_attatchment
 
+    def ensure_video
+        unless self.video_attatchment.attached?
+            errors[:video] << "Must be attached"
+        end
+    end
 end
--- frontend/components/videos/upload_video/upload.js
+++ frontend/components/videos/upload_video/upload.js
@@ -20,7 +20,8 @@ export default class Upload extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleInput = this.handleInput.bind(this);
         this.handleFile = this.handleFile.bind(this);
-        this.modalChanges = this.modalChanges.bind(this);
+        this.renderErrors = this.renderErrors.bind(this);
+        // this.modalChanges = this.modalChanges.bind(this);
     }
     handleInput(type) {
         return e => {
@@ -29,9 +30,9 @@ export default class Upload extends React.Component {
     }
     handleFile(e){
         const file = e.currentTarget.files[0];
-        if (file.size > 20000000){
-            this.setState({ photoFile: null, photoUrl: null})
-        } else {
+        // if (file.size > 20000000){
+        //     this.setState({ photoFile: null, photoUrl: null})
+        // } else {
             const fileReader = new FileReader();
             fileReader.onloadend = () => {
 
@@ -40,12 +41,12 @@ export default class Upload extends React.Component {
             if (file) {
                 fileReader.readAsDataURL(file);
             }
-        }
+        // }
             
         
     }
     renderErrors() {
-
+        debugger;
         if (this.props.errors instanceof Array || this.props.errors === null) {
             return (
                 []
@@ -74,7 +75,10 @@ export default class Upload extends React.Component {
         formData.append('video[user_id]', this.state.user_id);
         formData.append('video[date_created]', this.state.date_created);
         formData.append('video[view_count]', this.state.views);
-        formData.append('video[video_attatchment]', this.state.photoFile);
+        if(this.state.photoFile){
+            formData.append('video[video_attatchment]', this.state.photoFile);
+        }
+        
         // this.props.history.push("/")
         $.ajax({
             url: '/api/videos',
@@ -85,40 +89,41 @@ export default class Upload extends React.Component {
         });
         // this.props.history.push("/")
     }
-    modalChanges(){
-        // Get the modal
-        var modal = document.getElementById("upload-modal");
+    // modalChanges(){
+    //     // Get the modal
+    //     var modal = document.getElementById("upload-modal");
 
-        // Get the button that opens the modal
-        var btn = document.getElementById("Upload-form-info");
+    //     // Get the button that opens the modal
+    //     var btn = document.getElementById("Upload-form-info");
 
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
+    //     // Get the <span> element that closes the modal
+    //     var span = document.getElementsByClassName("close")[0];
 
-        debugger;
-        // When the user clicks the button, open the modal 
-        if (modal !== null){
-            btn.onclick = function () {
-                modal.style.display = "block";
-            }
+    //     debugger;
+    //     // When the user clicks the button, open the modal 
+    //     if (modal !== null){
+    //         btn.onclick = function () {
+    //             modal.style.display = "block";
+    //         }
 
-            // When the user clicks on <span> (x), close the modal
-            span.onclick = function () {
-                modal.style.display = "none";
-            }
+    //         // When the user clicks on <span> (x), close the modal
+    //         span.onclick = function () {
+    //             modal.style.display = "none";
+    //         }
 
-            // When the user clicks anywhere outside of the modal, close it
-            window.onclick = function (event) {
-                if (event.target == modal) {
-                    modal.style.display = "none";
-                }
-            }
-        }
+    //         // When the user clicks anywhere outside of the modal, close it
+    //         window.onclick = function (event) {
+    //             if (event.target == modal) {
+    //                 modal.style.display = "none";
+    //             }
+    //         }
+    //     }
         
-    }
+    // }
+    
 
     renderErrors() {
-
+        debugger;
         if (this.props.errors instanceof Array || this.props.errors === null) {
             return (
                 []
@@ -140,46 +145,92 @@ export default class Upload extends React.Component {
     }
 
     render() {
+        window.addEventListener('load', function () {
+
+            // Get the modal
+            var modal = document.getElementById("modal");
+
+            // Get the button that opens the modal
+            var btn = document.getElementById("Upload-form-info");
+
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+
+            // When the user clicks the button, open the modal 
+            btn.onclick = function () {
+                modal.style.display = "flex";
+            }
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function () {
+                modal.style.display = "none";
+            }
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function (event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+
+        });
         const preview = this.state.photoUrl ? 
             <video width="100%" height="100%" src={this.state.photoUrl} controls></video> : 
             <label for="filez" className="initial-upload-button-container"><i class="fas fa-upload"></i> &nbsp;&nbsp;<input className="hidden-button" id="filez" type="file" onChange={this.handleFile}></input><label className="initial-upload-button" for="filez">Choose A Video</label></label>;
         // debugger;
-        const preview_button = this.state.photoUrl ? <label className="alt-upload-button-container"><i class="fas fa-upload"></i>&nbsp;&nbsp;<input className="hidden-button" id="filez2" type="file" onChange={this.handleFile}></input><label for="filez2">Choose Another Video Instead</label></label> : null ;
+        const preview_button = this.state.photoUrl ? 
+        <div className="upload-button-group">
+            <label className="alt-upload-button-container">
+                <i class="fas fa-upload"></i>&nbsp;&nbsp;<input className="hidden-button" id="filez2" type="file" onChange={this.handleFile}></input>
+                <label for="filez2">Choose Another Video Instead</label>
+            </label>
+
+
+            
+        </div> : 
+        
+        null ;
         // this.modalChanges();
-        debugger;
         return (
             <form onSubmit={this.handleSubmit} className="upload-container">
                 <div className="upload-file-container">
                     {preview}
                 </div>
-                {preview_button}
-                <button type="button" id="Upload-form-info" onClick={this.modalChanges}>Fill Out Video Info</button>
                 
-            
-                <div id="upload-modal" className="upload-modal">
-                    <label>Title:
-                    <input type="text"
-                            className="upload-title"
-                            value={this.state.title}
-                            onChange={this.handleInput('title')}
-                        />
-                    </label>
+                {preview_button}
+                <button type="button" id="Upload-form-info" className="Upload-form-info">Fill Out Video Info</button>
+                
+                <div id="modal" className="modal">
+                    <div id="upload-modal" className="upload-modal">
+                        <span className="close">&times;</span>
+                        <div className="upload-information">
+                            <p className="upload-title">Title: </p>
+                            <input type="text"
+                                className="upload-title-info"
+                                value={this.state.title}
+                                onChange={this.handleInput('title')}
+                            />
+                         
 
-                    <label>Description:
-                    <textarea row="60" col="700"
-                            className="upload-description"
-                            value={this.state.description}
-                            onChange={this.handleInput('description')}
-                        />
-                        {/* <input type="file" onChange={this.handleFile}></input> */}
-                    </label>
-                    <span className="close">&times;</span>
+                            <p className="upload-description">Description: </p>
+                            <textarea row="500" col="1500"
+                                className="upload-description-info"
+                                value={this.state.description}
+                                onChange={this.handleInput('description')}
+                            />
+                                {/* <input type="file" onChange={this.handleFile}></input> */}
+                        </div>
+                        {this.renderErrors()}
+                        
+                    <input className="upload-submit" type="submit" value="Upload" />
+                    </div>
                 </div>
+                
                
-                {this.renderErrors()}
+                
 
 
-                <input className="upload-submit" type="submit" value="Upload" />
+                
            </form>
         )
     }
