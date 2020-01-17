268316a897567e8b3e852464fc7aedbf92531cdc
Anthony Chan <appianchan@gmail.com>
2020-01-16 16:46:22 -0800

    created error messages

--- frontend/components/videos/upload_video/upload.js
+++ frontend/components/videos/upload_video/upload.js
@@ -15,7 +15,8 @@ export default class Upload extends React.Component {
             title: "",
             description: "",
             photoFile: null,
-            photoUrl: null
+            photoUrl: null,
+            errors: []
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleInput = this.handleInput.bind(this);
@@ -47,17 +48,16 @@ export default class Upload extends React.Component {
     }
     renderErrors() {
         debugger;
-        if (this.props.errors instanceof Array || this.props.errors === null) {
+        if (this.state.errors instanceof Array || this.state.errors === null) {
             return (
                 []
             )
         } else {
             // debugger;
-            console.log(this.props.errors);
             return (
                 // this.props.errors.responseJSON
                 <ul>
-                    {this.props.errors.responseJSON.map((error, i) => (
+                    {this.state.errors.responseJSON.map((error, i) => (
                         <li key={`error-${i}`}>
                             {error}
                         </li>
@@ -80,13 +80,23 @@ export default class Upload extends React.Component {
         }
         
         // this.props.history.push("/")
-        $.ajax({
+        return $.ajax({
             url: '/api/videos',
             method: 'POST',
             data: formData,
             contentType: false,
             processData: false
-        });
+        }).then(
+            response => console.log(response.message),
+            response => this.setState({errors: response.responseJSON})
+        )
+        
+            
+            
+        //     errors => dispatch( {
+        //     type: "RECEIVE_SESSION_ERRORS",
+        //     errors
+        // }));
         // this.props.history.push("/")
     }
     // modalChanges(){
@@ -123,18 +133,17 @@ export default class Upload extends React.Component {
     
 
     renderErrors() {
-        debugger;
-        if (this.props.errors instanceof Array || this.props.errors === null) {
+       
+        if (this.state.errors === [] || this.state.errors === null) {
             return (
                 []
             )
         } else {
-            // debugger;
-            console.log(this.props.errors);
+            debugger;
             return (
                 // this.props.errors.responseJSON
                 <ul>
-                    {this.props.errors.responseJSON.map((error, i) => (
+                    {this.state.errors.map((error, i) => (
                         <li key={`error-${i}`}>
                             {error}
                         </li>
@@ -145,35 +154,8 @@ export default class Upload extends React.Component {
     }
 
     render() {
-        window.addEventListener('load', function () {
-
-            // Get the modal
-            var modal = document.getElementById("modal");
-
-            // Get the button that opens the modal
-            var btn = document.getElementById("Upload-form-info");
-
-            // Get the <span> element that closes the modal
-            var span = document.getElementsByClassName("close")[0];
-
-            // When the user clicks the button, open the modal 
-            btn.onclick = function () {
-                modal.style.display = "flex";
-            }
-
-            // When the user clicks on <span> (x), close the modal
-            span.onclick = function () {
-                modal.style.display = "none";
-            }
-
-            // When the user clicks anywhere outside of the modal, close it
-            window.onclick = function (event) {
-                if (event.target == modal) {
-                    modal.style.display = "none";
-                }
-            }
-
-        });
+        // this.renderErrors();
+        // debugger;
         const preview = this.state.photoUrl ? 
             <video width="100%" height="100%" src={this.state.photoUrl} controls></video> : 
             <label for="filez" className="initial-upload-button-container"><i class="fas fa-upload"></i> &nbsp;&nbsp;<input className="hidden-button" id="filez" type="file" onChange={this.handleFile}></input><label className="initial-upload-button" for="filez">Choose A Video</label></label>;
@@ -198,33 +180,33 @@ export default class Upload extends React.Component {
                 </div>
                 
                 {preview_button}
-                <button type="button" id="Upload-form-info" className="Upload-form-info">Fill Out Video Info</button>
                 
-                <div id="modal" className="modal">
-                    <div id="upload-modal" className="upload-modal">
-                        <span className="close">&times;</span>
-                        <div className="upload-information">
-                            <p className="upload-title">Title: </p>
-                            <input type="text"
-                                className="upload-title-info"
-                                value={this.state.title}
-                                onChange={this.handleInput('title')}
-                            />
-                         
-
-                            <p className="upload-description">Description: </p>
-                            <textarea row="500" col="1500"
-                                className="upload-description-info"
-                                value={this.state.description}
-                                onChange={this.handleInput('description')}
-                            />
-                                {/* <input type="file" onChange={this.handleFile}></input> */}
-                        </div>
-                        {this.renderErrors()}
+                
+                
+                <div id="upload-modal" className="upload-modal">
+                    <div className="upload-information">
+                        <p className="upload-title">Title: </p>
+                        <input type="text"
+                            className="upload-title-info"
+                            value={this.state.title}
+                            onChange={this.handleInput('title')}
+                        />
                         
-                    <input className="upload-submit" type="submit" value="Upload" />
+
+                        <p className="upload-description">Description: </p>
+                        <textarea row="500" col="1500"
+                            className="upload-description-info"
+                            value={this.state.description}
+                            onChange={this.handleInput('description')}
+                        />
+                            {/* <input type="file" onChange={this.handleFile}></input> */}
                     </div>
+                    {/* {this.state.errors} */}
+                    {this.renderErrors()}
+                    
+                <input className="upload-submit" type="submit" value="Upload" />
                 </div>
+            
                 
                
                 
